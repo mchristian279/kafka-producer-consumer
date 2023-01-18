d23608bc889d74b799b5d27cfa8b09d782398faf
Michael Christian <m.christian279@gmail.com>
2023-01-18 19:25:56 -0300

    refactor: variable consumer_group
    refactor (variable/consumer_group): variable to use name of topic Kafka and generate random id to use with consumer-group name

--- kafka-consumer.py
+++ kafka-consumer.py
@@ -1,22 +1,28 @@
 from kafka import KafkaConsumer
 from json import loads
+import random
 
-print('Kafka consumer starting...')
+#Generate ID Consumer-Group
+id = random.randint(1,1000)
+
+#Name Consumer-Group Kafka
+consumer_group = ('consumer-group-name-'+str(id))
+
+print('Kafka Starting...')
 
 topic = 'topic-name'
 
 consumer = KafkaConsumer(
     topic,
-    bootstrap_servers='server:port,server:port',
+    bootstrap_servers='server:port,server:port,
     security_protocol='SSL',
     ssl_check_hostname=False,
     ssl_cafile='./cacert.pem',
     ssl_certfile='./certificate.pem',
     auto_offset_reset='earliest',
     enable_auto_commit=True,
-    group_id='group-id-name'
-    )
+    group_id=consumer_group
+)
 # Read data from kafka
 for message in consumer:
- print (message)
-
+    print(message)
